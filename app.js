77626f271749b3f1aae751e023ebd1eec7cc3c89
Miihy <hean7321@naver.com>
2024-04-22 20:34:55 +0900

    feat : 채팅화면 구현 및 css 추가

--- app.js
+++ app.js
@@ -13,8 +13,16 @@ const io = socketIO(server);
 app.use(express.static(path.join(__dirname,"src")));
 const PORT = process.env.PORT || 5000;
 
-io.on("connection",(socket)=>{
-    console.log("연결 성공!");
+io.on("connection",(socket) => {
+    //console.log("연결 성공!");
+
+    socket.on("chatting", (data) => {
+
+        //Object 형식으로 data를 받음
+        //console.log(data);
+        
+        io.emit("chatting", data);
+    });
 });
 
 server.listen(PORT, ()=>console.log(`server is running ${PORT}`));
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,1 +1,27 @@
-console.log('hello js');
+"use strict"
+
+//클라이언트의 socket.io가 담김
+const socket = io();
+
+const nickName = document.querySelector("#nickname");
+const chatList = document.querySelector(".chatting-list");
+const chatInput = document.querySelector(".chatting-input");
+const sendBtn = document.querySelector(".send-btn");
+
+sendBtn.addEventListener("click", () => {
+    const param = {
+        name : nickName.value,
+        msg : chatInput.value
+    }
+    socket.emit("chatting", param);
+
+});
+
+socket.on("chatting", (data) => {
+    //console.log(data);
+    const li = document.createElement("li");
+    li.innerText = `${data.name}님이 - ${data.msg}`;
+    chatList.appendChild(li);
+});
+
+console.log(socket);
