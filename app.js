02526ec27305603d11ac8886ca14f0d628766a0c
Miihy <hean7321@naver.com>
2024-04-22 19:39:02 +0900

    feat : Websocket 연결 확인

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const path = require("path");
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+
+//프로젝트 폴더경로가 출력됨
+//console.log(__dirname);
+
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname,"src")));
+const PORT = process.env.PORT || 5000;
+
+io.on("connection",(socket)=>{
+    console.log("연결 성공!");
+});
+
+server.listen(PORT, ()=>console.log(`server is running ${PORT}`));
